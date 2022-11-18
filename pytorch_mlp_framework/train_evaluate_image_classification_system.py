970c19a472e1a79566a4557591879dcbe3ca5221
Johnnyliii <117656994+Johnnyliii@users.noreply.github.com>
2022-11-18 17:54:05 +0000

    added learning_rate for classification_system

--- pytorch_mlp_framework/train_evaluate_image_classification_system.py
+++ pytorch_mlp_framework/train_evaluate_image_classification_system.py
@@ -61,7 +61,8 @@ custom_conv_net = ConvolutionalNetwork(  # initialize our network object, in thi
     num_output_classes=args.num_classes, num_filters=args.num_filters, use_bias=False,
     num_blocks_per_stage=args.num_blocks_per_stage, num_stages=args.num_stages,
     processing_block_type=processing_block_type,
-    dimensionality_reduction_block_type=dim_reduction_block_type)
+    dimensionality_reduction_block_type=dim_reduction_block_type,
+    learning_rate = args.learning_rate)
 
 conv_experiment = ExperimentBuilder(network_model=custom_conv_net,
                                     experiment_name=args.experiment_name,
@@ -70,6 +71,6 @@ conv_experiment = ExperimentBuilder(network_model=custom_conv_net,
                                     use_gpu=args.use_gpu,
                                     continue_from_epoch=args.continue_from_epoch,
                                     train_data=train_data_loader, val_data=val_data_loader,
-                                    test_data=test_data_loader)  # build an experiment object
-conv_experiment = conv_experiment.to(torch.device('cuda'))
+                                    test_data=test_data_loader,
+                                    learning_rate = args.learning_rate)  # build an experiment object
 experiment_metrics, test_metrics = conv_experiment.run_experiment()  # run experiment and return experiment metrics
